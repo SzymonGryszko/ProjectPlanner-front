4515273dc4b2e8265abcc547785e53e30344d887
Szymon Gryszko <gryszkoszymon@gmail.com>
2020-12-30 13:58:10 +0100

    routing to board and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BoardComponent } from './board/board.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    
+  //Site routes goes here 
+  { 
+      path: '', 
+      component: MainComponent,
+      children: [
+        { path: 'board/:id', component: BoardComponent, pathMatch: 'full'}
+      ]
+  },
+  { path: 'login', component: LoginComponent},
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BoardComponent } from './board/board.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { LoginComponent } from './login/login.component';
     BoardComponent,
     HeaderComponent,
     LoginComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
